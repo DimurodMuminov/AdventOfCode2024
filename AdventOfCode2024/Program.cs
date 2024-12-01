ad0d227bb584273a7f12dd80bfe100e9cea3d3a2
Dilmurod <dm4060142@gmail.com>
2024-12-01 12:45:47 +0500

    sort list ; count until equals changing; result=count*list1(i)

--- AdventOfCode2024/Program.cs
+++ AdventOfCode2024/Program.cs
@@ -33,12 +33,36 @@
             }
             list1.Sort();
             list2.Sort();
-
+            /*
+             *Day 1 part1
             for (int i=0;i<list2.Count;i++)
             {
                 result+=Math.Abs(list1.ElementAt(i)-list2.ElementAt(i));
-            }
+            }*/
 
+            //Day 1 part 2
+
+            int count = 0;
+            bool change=false;
+
+            for (int i = 0; i < list1.Count; i++)
+            {
+                for (int j = 0; j < list1.Count; j++)
+                {
+                    if (list1[i] == list2[j])
+                    {
+                        count++;
+                        change = true;
+                    }
+                    else if (change)
+                    {
+                        break;
+                    }
+                }
+                result += (count * list1[i]);
+                count = 0;
+                change = false;
+            }
         }
         catch
         {
